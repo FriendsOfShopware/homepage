977289dbe98a573c32abeaf21eb4470550125a89
leichteckig <29896429+leichteckig@users.noreply.github.com>
2020-07-24 00:54:20 +0200

    Implement component for top contributors

---
+++ src/components/Contributor.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="contributor">
+    <div class="avatar column">
+      <img class="avatar-image" src="../assets/frosh-logo.png" alt="Friends of Shopware Logo" />
+    </div>
+    <div class="name column">
+      <span class="fullname">{{ user.name }}</span>
+      <span class="nickname">{{ user.user }}</span>
+    </div>
+    <div class="tag column">
+      {{ user.contributionCount }} contributions
+    </div>
+    <div class="tag column">
+      {{ user.commitCount }} commits
+    </div>
+    <div class="tag column">
+      {{ user.prCount }} pull requests
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Contributor",
+    props: {
+      user: Object
+    },
+
+    computed: {
+      imgSource() {
+        return `../${this.user.image}`
+      }
+    },
+
+    created() {
+      console.log(this.user);
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+  .contributor {
+    display: inline-flex;
+    border-radius: 10px;
+    border: 1px solid $border;
+    padding: $default-margin;
+    margin: $default-margin;
+    height: inherit;
+    width: 100%;
+  }
+
+  .column {
+    margin: $default-margin;
+  }
+
+  .name {
+    display: flex;
+    flex-direction: column;
+    flex: 1 0;
+    height: inherit;
+
+    .fullname {
+      font-weight: bold;
+      font-size: 1.2rem;
+    }
+
+    .nickname {
+      color: rgba($font, 0.7);
+      font-size: 0.9rem;
+      line-height: 1.3rem;
+      margin: calc(#{$default-margin} / 2) 0;
+      white-space: initial;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      justify-content: center;
+      height: 100%;
+    }
+  }
+
+  .avatar-image {
+    border-radius: 50%;
+    width: 5em;
+  }
+
+</style>
---
+++ src/components/TopContributors.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="top-contributors">
+    <h3 class="headline">Top Contributors</h3>
+    <div class="top-list">
+      <div v-for="user in contributors" :key="user.id">
+        <Contributor class="contributor-view" :user="user"></Contributor>
+      </div>
+      <div class="fade-out"></div>
+    </div>
+    <div class="further-information">
+      <FroshButton href="#">Find contributors on Github</FroshButton>
+      <FroshButton href="#" variant="blank">How to contribute</FroshButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import topContributors from "./../dataSets/topContributors";
+import Contributor from "./Contributor";
+import FroshButton from "./FroshButton";
+
+export default {
+  name: "TopContributors",
+  components: {
+    FroshButton,
+    Contributor
+  },
+  data() {
+    return {
+      contributors: topContributors
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.top-contributors {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  position: relative;
+  max-width: 100%;
+}
+
+.fade-out {
+  position: absolute;
+  bottom: 0;
+  height: 100%;
+  width: 100%;
+
+  background: linear-gradient(to bottom,
+    rgba(0,0,0, 0) 10%,
+    $background 85%
+  );
+}
+
+.headline {
+  font-size: 1.75rem;
+  font-weight: bold;
+  width: 100%;
+  height: 36px;
+  text-align: center;
+}
+
+.top-list {
+  width: 90%;
+  height: 100%;
+}
+
+.further-information {
+  padding: calc(#{$default-padding} * 2) 0;
+  width:70%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  position: relative;
+}
+</style>
